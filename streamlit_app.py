062ed610c815c3465011a513c63cf014d2f931d7
Alicia Chen <aliciachen10@gmail.com>
2023-10-07 23:32:20 -0700

    feedback instructions

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,6 +75,8 @@ When offering information or feedback, we want to avoid triggering discord. We c
     """,
     """
 Now that you know these principles, let's practice motivating a 30-year-old depressed patient using the elicit-provide-elicit framework. If you like, you can go back to the previous screens and copy-paste information or take notes on a notepad file, and refer to that during your conversation. You will roleplay the role of peer counselor trained in MI, and you'll be talking to a 30-year old patient. Begin by using the elicit-provide-elicit framework, asking the client what ideas she has for how to feel better when she feels depressed. Then, continue to use the framework as you talk to her.
+
+You can press "Switch to feedback mode" when you're finished wrapping up the conversation and want feedback on how it went.
 """
 ]
 
